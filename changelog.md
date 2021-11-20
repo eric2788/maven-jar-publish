45acf8d1f64c5ad1486f014366c475e5df4ad7f3
eric2788 <tsukiko852@gmail.com>
2021-11-20 18:36:29 +0800

    created example project

---
+++ changelog.md
@@ -0,0 +1,10 @@
+This is an update log
+with multiple line
+support....
+
+### Update
+
+- added magic
+- fixed not work
+
+etc.
---
+++ src/main/java/com/ericlam/example/App.java
@@ -0,0 +1,10 @@
+package com.ericlam.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
